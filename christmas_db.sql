e72a1474cf73f37f88d0a9d357b46d666884ce6c
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-03 12:01:02 -0500

    created christmas db and people table using migration scripts

---
+++ christmas_db.sql
@@ -0,0 +1,29 @@
+
+
+USE christmas_db;
+DROP TABLE IF EXISTS people;
+
+CREATE TABLE IF NOT EXISTS
+    people (
+               id int(10) unsigned NOT NULL AUTO_INCREMENT,
+               first_name varchar(50) NOT NULL,
+               last_name varchar(50) NOT NULL,
+               age int UNSIGNED DEFAULT NULL,
+               birthday varchar(50) NOT NULL,
+               nice tinyint NOT NULL,
+               PRIMARY KEY (id)
+);
+
+use christmas_db;
+
+INSERT INTO people (first_name, last_name, age, birthday, nice)
+VALUES ('Michael','Angelo',32,'02/25/1987',true);
+
+INSERT INTO people (first_name, last_name, age, birthday, nice)
+VALUES ('Donna','Tello',28,'05/30/1991',true);
+
+INSERT INTO people (first_name, last_name, age, birthday, nice)
+VALUES ('Leo','Nardo',34,'02/17/1985',false);
+
+INSERT INTO people (first_name, last_name, age, birthday, nice)
+VALUES ('Raph','Fayel',25,'10/01/1995',true);
