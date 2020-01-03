92b323562fd42d4e133ac670f51925c322363622
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-03 11:36:38 -0500

    arrays, adding reindeer to existing aray

--- src/christmas/Reindeer.java
+++ src/christmas/Reindeer.java
@@ -8,15 +8,16 @@ package christmas;
 public class Reindeer {
     protected String name;
 
+    public String getName() {
+        return name;
+    }
+
     public Reindeer(String name) {
         this.name = name;
         if (name.equalsIgnoreCase("rudolph")) {
             System.out.println(name + " has a shiny red nose!");
         } else {
-            System.out.println("This reindeer is named " + name);
+            System.out.println("This reindeer is named " + name + ".");
         }
     }
-
-
-
 }
--- src/christmas/Santa.java
+++ src/christmas/Santa.java
@@ -1,5 +1,6 @@
 package christmas;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //Create a new Java package called Christmas.
@@ -8,8 +9,18 @@ import java.util.Scanner;
 //        Store the response in a String variable.
 //        Bonus: Continue to ask if the user enters an empty String
 
+//Going back into your Santa class, create a public static Array property that stores 9 (or 10) instances of Reindeer objects.
+//        go back into your main method and give each Reindeer instance a position in the Array you just created.
+//        December 31st
+//        What if we want to hire more Reindeer in the future and add them to our Array?
+//        In your Santa class, create a pubilc static addReindeer method that would allow us to add to our Array.
+//        Remember that Arrays in Java have a set length, so this method will actually need to create a copy with a different lenght.
+//        Need a hint? Go back to our ArraysExercises from Java II.
+
 
 public class Santa {
+    Reindeer[] daDeers;
+
 
     public static void ask() {
         Scanner scan = new Scanner(System.in);
@@ -22,18 +33,43 @@ public class Santa {
             System.out.println("Oh... you'd like " + gift);
         }
     }
+
+
+    public static Reindeer[] addReindeer(Reindeer[] currentArr, Reindeer ReindeerName){
+            Reindeer[] tempArr = Arrays.copyOf(currentArr, currentArr.length+1);
+            tempArr[currentArr.length] = ReindeerName;
+            return tempArr;
+        }
+
+
+
+
     public static void main(String[] args) {
 
-        Reindeer dasher = new Reindeer("Dasher");
-        Reindeer dancer = new Reindeer("Dancer");
-        Reindeer prancer = new Reindeer("Prancer");
-        Reindeer vixen = new Reindeer("Vixen");
-        Reindeer comet = new Reindeer("Comet");
-        Reindeer cupid = new Reindeer("Cupid");
-        Reindeer donner = new Reindeer("Donner");
-        Reindeer blitzen = new Reindeer("Blitzen");
-        Reindeer rudolph = new Reindeer("Rudolph");
-        Reindeer olive = new Reindeer("Olive");
+
+
+
+
+       Reindeer[] daDeers = new Reindeer[10];
+
+       daDeers[0] = new Reindeer("Dasher");
+       daDeers[1] = new Reindeer("Dancer");
+       daDeers[2] = new Reindeer("Prancer");
+       daDeers[3] = new Reindeer("Vixen");
+       daDeers[4] = new Reindeer("Comet");
+       daDeers[5] = new Reindeer("Cupid");
+       daDeers[6] = new Reindeer("Donner");
+       daDeers[7] = new Reindeer("Blitzen");
+       daDeers[8] = new Reindeer("Rudolph");
+       daDeers[9] = new Reindeer("Olive");
+
+       daDeers = addReindeer(daDeers, new Reindeer("Bill"));
+       daDeers = addReindeer(daDeers, new Reindeer("Ted"));
+
+        for (Reindeer deer : daDeers){
+            System.out.println(deer.getName());
+        }
+
         ask();
 
 
